af760c799fb82c01b74c8f3a40e67acc5db5c205
adovg <kroatoan@i.ua>
2025-04-11 17:28:40 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
- import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
- // https://vitejs.dev/config/
- export default defineConfig({
-   base: '',
- })
- 
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler", // or "modern"
+      },
+    },
+  },
+});
