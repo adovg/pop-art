a0dd4c1c8e933ee704db04ecdf2c2d75970fc86c
adovg <kroatoan@i.ua>
2025-04-23 22:12:17 +0300

    add trasnlate

--- src/lang.js
+++ src/lang.js
@@ -3,11 +3,43 @@ const translations = {
     about: "About",
     visuals: "Visual Materials",
     text: "Text",
+    hero__title_1: "American Pop Art",
+    hero__title_2: "and its",
+    hero__title_3: "modern",
+    hero__title_4: "models",
+    about__title_1: "Evolution of American Pop Art",
+    about__title_2: "Global Impact",
+    about__title_3: "Digital & Post-Digital Transformation",
+    about__title_4: "Socio-Cultural & Civic Engagement",
+    about__text_1:
+      "Mid-20th-century Pop Art (e.g., Rauschenberg, Warhol, Lichtenstein) revolutionized artistic paradigms through mass culture, democratization, and synthetic styles. Key techniques: collage, assemblage, multimedia installations blending avant-garde traditions with postmodern innovation.",
+    about__text_2:
+      "Pop Art’s influence spread to Europe (e.g., German Neo-Dada), Asia(Japanese adaptations), and Latin America, often merging with local traditions. Examples: Damien Hirst’s provocative installations (UK), Kehinde Wiley’s postmodern portraiture (USA), and digital art movements worldwide.",
+    about__text_3:
+      "Contemporary artists (e.g., Refik Anadol, Krista Kim) use AI, VR, and interactive media to create immersive, multi-sensory experiences. Themes: Digital humanism, environmental activism (e.g., Anadol’s Glacial Dream), and redefining art’s role in a tech-driven society.",
+    about__text_4:
+      "Artists like Wiley and Mickalene Thomas address identity, race, and social justice through reimagined classical techniques and pop art aesthetics. Exhibitions like Pop Forever (Louis Vuitton Foundation, 2024–25) highlight Pop Art’s enduring relevance.",
   },
   ua: {
     about: "Про нас",
     visuals: "Візуальні матеріали",
     text: "Текст",
+    hero__title_1: "Американський поп арт",
+    hero__title_2: "та його",
+    hero__title_3: "сучасні",
+    hero__title_4: "моделі",
+    about__title_1: "Еволюція американського поп-арту",
+    about__title_2: "Глобальний вплив",
+    about__title_3: "Цифрова та постцифрова трансформація",
+    about__title_4: "Соціально-культурна та громадянська активність",
+    about__text_1:
+      "Поп-арт середини 20-го століття (наприклад, Раушенберг, Воргол, Ліхтенштейн) революціонізував художні парадигми через масову культуру, демократизацію та синтетичні стилі. Ключові техніки: колаж, асамбляж, мультимедійні інсталяції, що поєднують авангардні традиції з постмодерністськими інноваціями.",
+    about__text_2:
+      "Вплив поп-арту поширився на Європу (наприклад, німецький неодадаїзм), Азію (японські адаптації) та Латинську Америку, часто зливаючись з місцевими традиціями. Приклади: провокаційні інсталяції Демієна Херста (Велика Британія), постмодерністські портрети Кенінде Вайлі (США) та цифрові мистецькі рухи по всьому світу.",
+    about__text_3:
+      "Сучасні митці (наприклад, Рефік Анадол, Кріста Кім) використовують штучний інтелект, віртуальну реальність та інтерактивні медіа для створення захопливих, мультисенсорних вражень. Теми: цифровий гуманізм, екологічний активізм (наприклад, «Льодовикова мрія Анадола») та переосмислення ролі мистецтва в технологічно керованому суспільстві.",
+    about__text_4:
+      "Такі митці, як Вайлі та Мікалін Томас, звертаються до питань ідентичності, раси та соціальної справедливості за допомогою переосмислення класичних технік та естетики поп-арту. Виставки, такі як «Pop Forever» (Фонд Луї Віттона, 2024–2025), підкреслюють незмінну актуальність поп-арту.",
   },
 };
 
